4b3102403adf8e0451bd225e82d01be3ce3ee05d
Maximilian Unterberger <maxi@unterberger.cc>
2025-11-18 09:38:11 +0100

    file write

--- webscraper.py
+++ webscraper.py
@@ -1,19 +1,22 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-url = "https://quotes.toscrape.com/"
+for page in range(1, 10):
+    url = "https://quotes.toscrape.com/" + f"page/{page}/" 
 
-response = get(url)
+    response = get(url)
 
-soup = BeautifulSoup(
-    response.content,
-    "html.parser"
-)
+    soup = BeautifulSoup(
+        response.content,
+        "html.parser"
+    )
 
-quotes = soup.find_all(
-    "div",
-    attrs={"class": "quote"}
-)
+    quotes = soup.find_all(
+        "div",
+        attrs={"class": "quote"}
+    )
 
-for index, quote in enumerate(quotes):
-    print(index, quote.text)
+    for index, quote in enumerate(quotes):
+        with open(f"./quotes/quote_{index}.txt","a") as file: 
+            file.write(f"{index} {quote.text}")
+        print(index, quote.text)
